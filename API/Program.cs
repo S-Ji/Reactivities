a39be337e8ebdf7eb29ae987a07c68a1e344a08e
S-Ji <kietminh39@gmail.com>
2020-12-08 05:36:02 +0700

    End of S5

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +22,8 @@ namespace API
                 var services = scope.ServiceProvider;
                 try{
                     var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
                 catch (Exception ex){
                     var logger = services.GetRequiredService<ILogger<Program>>();
