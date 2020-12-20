519cc0be021e7c8fde61608bf5bae24a3fe335e6
S-Ji <kietminh39@gmail.com>
2020-12-21 05:01:07 +0700

    End of S8

--- client-app/src/app/stored/activityStore.ts
+++ client-app/src/app/stored/activityStore.ts
@@ -7,9 +7,7 @@ configure({enforceActions: 'always'})
 
 class ActivityStore {
     @observable activityRegistry = new Map();
-    @observable activities: IActivity[] = [];
-    @observable selectedActivity: IActivity | undefined;
-    @observable editMode = false;
+    @observable activity: IActivity | null = null;
     @observable loadingInitial = false;
     @observable submitting = false;
     @observable target = '';
@@ -39,13 +37,42 @@ class ActivityStore {
         }
     };
 
+    @action loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if (activity) {
+            this.activity = activity;
+        } else {
+            this.loadingInitial = true;
+            try {
+                activity = await agent.Activities.details(id);
+                runInAction(() => {
+                    this.activity = activity;
+                    this.loadingInitial = false;
+                })
+            } catch(error) {
+                runInAction(() => {
+                this.loadingInitial = false;
+                })
+                console.log(error);
+                
+            }
+        }
+    }
+
+    @action clearActivity = () => {
+        this.activity = null;
+    }
+
+    getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
     @action createActivity = async (activity: IActivity) => {
         this.submitting = true;
         try {
             await agent.Activities.create(activity);
             runInAction(() => { 
                 this.activityRegistry.set(activity.id, activity)
-                this.editMode = false;
                 this.submitting = false;
             })
 
@@ -64,8 +91,7 @@ class ActivityStore {
             await agent.Activities.update(activity);
             runInAction(() => { 
             this.activityRegistry.set(activity.id, activity);
-            this.selectedActivity = activity;
-            this.editMode = false;
+            this.activity = activity;
             this.submitting = false;
             })
         } catch(error) {
@@ -75,19 +101,6 @@ class ActivityStore {
             console.log(error);
             
         }
-    }
-
-    @action openEditForm = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-        this.editMode = true;
-    }
-
-    @action cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    @action cancelFormOpen = () => {
-        this.editMode = false;
     }
 
     @action deleteActivity = async (event:SyntheticEvent<HTMLButtonElement> ,id:string) => {
@@ -109,16 +122,6 @@ class ActivityStore {
             
         }
     }
-
-    @action openCreateForm = () => {
-        this.editMode = true;
-        this.selectedActivity = undefined;
-    }
-
-    @action selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-        this.editMode = false;
-    };
 
     constructor() {
         makeAutoObservable(this)
--- client-app/src/features/Nav/NavBar.tsx
+++ client-app/src/features/Nav/NavBar.tsx
@@ -1,22 +1,23 @@
 import { observer } from 'mobx-react-lite';
-import React, { useContext } from 'react'
+import React from 'react'
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu, MenuItem } from 'semantic-ui-react'
-import ActivityStore from '../../app/stored/activityStore';
+
 
 const NavBar: React.FC = () => {
-    const activityStore = useContext(ActivityStore);
     return (
         <Menu fixed='top' inverted>
             <Container>
-            <MenuItem header>
+            <MenuItem header as={NavLink} exact to='/' >
                 <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
                 Reactivities
                  </MenuItem>
         <Menu.Item
           name='Avtivities'
+          as={NavLink} to='/activities'
         />
         <Menu.Item>
-            <Button onClick={activityStore.openCreateForm} positive content='Create Activity' />
+            <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
         </Menu.Item>
             </Container>
         
